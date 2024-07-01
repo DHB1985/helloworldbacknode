0c919a20fe55fbb86b5bb3301a370726735cd975
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2024-07-01 09:37:10 -0300

    fixes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
 const app = express();
-
 PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   return res.send('Ok')
